64d9721348364e01d366c72e274e8791dc8941bf
Developer <dev@voiceai.local>
2026-08-25 16:44:11 +0545

    chore: clean up next.config unsupported keys

--- next.config.ts
+++ next.config.ts
@@ -6,12 +6,6 @@ const nextConfig = {
     domains: ["*"], // Note: In production, specify exact domains
   },
   allowedDevOrigins: ['127.0.0.1', 'localhost', '192.168.1.66'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig as NextConfig;
