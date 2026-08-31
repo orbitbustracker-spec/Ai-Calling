86a8832f404a43398bb3797912129aea88365e23
Developer <dev@voiceai.local>
2026-08-31 07:20:30 +0545

    chore: ignore eslint during build to prevent vercel failing on scratch files

--- test_find.js
+++
@@ -1,9 +0,0 @@
-
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const all = await prisma.pendingRegistration.findMany();
-  console.log(all.map(a => a.email));
-}
-main().finally(() => prisma.$disconnect());
-
